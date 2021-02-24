546e7bda8e89ada80569532a715b60be626d7dec
rachel <baruh.rahel@gmail.com>
2021-02-24 10:41:33 +0200

    17-2

--- 17-2/index.js
+++ 17-2/index.js
@@ -2,6 +2,6 @@ const msg = document.querySelector("#msg1")
 const el = document.querySelector("Submit")
 el.addEventListener("click", () => {
     if (msg.length <= 10) {
-        document.getElementById("alert").innerHTML = "enter at least 100 letter";
+        document.getElementById("#alert").innerHTML = "enter at least 100 letter";
     }
 })
